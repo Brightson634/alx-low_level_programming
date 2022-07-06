e4243af565193cd76c10d3554fb3854fa9f94127
Brightson634 <brightsontwine@gmail.com>
2022-07-06 14:55:16 -0700

    simple

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _abs - Computes the absolute value of an integer
+ * @n: integer to be checked
+ *
+ * Return: the absolute value
+ */
+
+int _abs(int n)
+{
+	return ((n <= 0) ? n * -1 : n);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -3,4 +3,6 @@ void print_alphabet_x10(void);
 int _islower(int c);
 int print_sign(int n);
 int _isalpha(int c);
+int _abs(int n);
+int print_last_digit(int var);
 void print_alphabet(void);
