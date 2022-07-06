3a63df8a184d5e700eaae5ab8513bbdd4e6c6b3e
Brightson634 <brightsontwine@gmail.com>
2022-07-06 11:58:39 -0700

    Message to print  putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+
+/**
+*_putchar-a program that prints _putchar, followed by a new line.
+ * @c: The character to print
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+*/
+int _putchar(char c)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+}
