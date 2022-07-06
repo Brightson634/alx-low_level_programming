749fc8534d4fdbcb21a8437f2de140da4641b0b5
Brightson634 <brightsontwine@gmail.com>
2022-07-06 12:11:09 -0700

    Fixes

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,11 +3,8 @@
 
 /**
 *_putchar-a program that prints _putchar, followed by a new line.
- * @c: The character to print
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
 */
-int _putchar(char c)
+void main(void)
 {
 _putchar('_');
 _putchar('p');
