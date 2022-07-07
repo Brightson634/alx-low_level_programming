2e2a3a72e5ca9f9e21cde7aa97c1bf5eeae7f58c
Brightson634 <brightsontwine@gmail.com>
2022-07-07 16:10:43 -0700

    loops

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,10 @@
 int _putchar(char c);
 void print_alphabet_x10(void);
+void print(int n);
+void jack_bauer(void);
+void print_to_98(int n);
+int add(int, int);
+void times_table(void);
 int _islower(int c);
 int print_sign(int n);
 int _isalpha(int c);
