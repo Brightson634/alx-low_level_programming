f11b41a7fa292f2d1da1d1c72e1bebd99cab21fa
Brightson634 <brightsontwine@gmail.com>
2022-07-08 03:35:10 -0700

    10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,20 +1,11 @@
-nclude "main.h"
-
-
+#include "main.h"
 
 /**
-
  * add - add 2 integers
-
  * @var1: first integer
-
  * @var2: second integer
-
  * Return: the integer sum of var1 and var2
-
  */
-
-
 
 int add(int var1, int var2)
 
