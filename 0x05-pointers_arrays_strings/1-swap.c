5f6526d4d8d241ae8926a58c85f66144860c4e26
Brightson634 <brightsontwine@gmail.com>
2022-07-12 14:11:32 -0700

    swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* swap_int - swap two integers
+* @a: variable one
+* @b: variable two
+*/
+	void swap_int(int *a, int *b)
+	{
+
+	int n = *a;
+	*a  = *b;
+	*b = n;
+
+	}
+
