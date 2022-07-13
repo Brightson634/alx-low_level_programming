727804b7d996db546aadb784dcb211ab8dd1789a
Brightson634 <brightsontwine@gmail.com>
2022-07-13 01:52:26 -0700

    reverse string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * print_rev - print string in reversed order
+ * @str: string to be reversed
+ */
+
+void print_rev(char *s)
+{
+int i, len = strlen(str);
+
+
+	for (i = len - 1; i >= 0; i--)
+
+		_putchar(str[i]);
+
+
+	_putchar('\n');
+}
