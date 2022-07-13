09e7bf58a1682b22c0bf4d38810f68338605a311
Brightson634 <brightsontwine@gmail.com>
2022-07-13 00:28:40 -0700

    fixing str len

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -4,12 +4,10 @@
 * _strlen - print the length of a given string
 *
 *@s: given pointer string
-*@gstr: length of the given string
 */
 	int _strlen(char *s)
 	{
-	int gstr;
 
 	s = ("Hello there! I love Alx programming");
-	gstr = _strlen(s);
+	 _strlen(s);
 	}
