099e070a1234b8102a568ccd4ce507d63413ebf8
Brightson634 <brightsontwine@gmail.com>
2022-07-13 00:25:57 -0700

    string length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* _strlen - print the length of a given string
+*
+*@s: given pointer string
+*@gstr: length of the given string
+*/
+	int _strlen(char *s)
+	{
+	int gstr;
+
+	s = ("Hello there! I love Alx programming");
+	gstr = _strlen(s);
+	}
