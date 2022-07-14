409b43ecb23f12610c265736bb5da5bd6ae2018f
Brightson634 <brightsontwine@gmail.com>
2022-07-14 09:52:38 +0300

    pgit commit --amend --author="Brightson634 <brightsontwine@gmail.com>project 06 pointers

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * strcat -concatinates strings
+ * @src: The source string to be appended to @dest.
+ * @dest: for charcter
+ * Return: A pointer to the destination string @dest.
+ */
+char *strcat(char *dest, const char *src)
+{
+	int index = 0, dest_len = 0;
+
+	while (dest[index++])
+		dest_len++;
+
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
+
+	return (dest);
+}
