f619e68dad771e7bb26ed67e281cc138978e79f7
Brightson634 <brightsontwine@gmail.com>
2022-07-13 17:06:08 -0700

    Fixing the 100 times table

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,6 +2,7 @@ int _putchar(char c);
 void print_alphabet_x10(void);
 void jack_bauer(void);
 void print_to_98(int n);
+void print_times_table(int n);
 int add(int, int);
 void times_table(void);
 int _islower(int c);
