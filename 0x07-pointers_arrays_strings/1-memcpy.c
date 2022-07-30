6f592d2bacf2d64d98d094e04a5c042574d04009
Brightson634 <brightsontwine@gmail.com>
2022-07-30 13:40:14 -0700

    Just afew fixes in main.h

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,16 +1,14 @@
-
 #include "main.h"
 
 /**
- * *_memcpy - Copies @n bytes from the memory area pointed
+ * _memcpy - Copies @n bytes from the memory area pointed
  *           to by @src into that pointed to by @dest.
  * @dest: A pointer to the memory area to copy @src into.
  * @src: The source buffer to copy characters from.
  * @n: The number of bytes to copy from @src.
- *
  * Return: A pointer to the destination buffer @dest.
  */
-void *_memcpy(void *dest, const void *src, size_t n)
+void *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int index;
 	unsigned char *destination = dest;
