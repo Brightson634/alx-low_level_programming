51b1e1dab4884d9db409585838c0700bc3ab9801
Brightson634 <brightsontwine@gmail.com>
2022-07-30 10:59:01 -0700

    Description update

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,8 +1,7 @@
 #include "main.h"
 
 /**
- * memset - Fills the first n bytes of the memory area
- *          pointed to by @s with the constant byte @c.
+ * _memset - Fills the first n bytes of the memory area pointed to by @s with the constant byte @c.
  * @s: A pointer to the memory area to be filled.
  * @c: The character to fill the memory area with.
  * @n: The number of bytes to be filled.
